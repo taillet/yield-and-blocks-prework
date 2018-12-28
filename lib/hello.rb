4894d3c6c1c3e96940c801ae92221db2b57241a9
Heloise Taillet <ht2394@columbia.edu>
2018-12-28 18:12:47 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,12 @@
 def hello_t
   i = 0
-  
-   while i < array.length
-     yield array[i]
-     i = i + 1
-   end
+ 
+  while i < array.length
+    yield(array[i])
+    i = i + 1
+  end
+ 
+  array
 end
 
 # call your method here!
-
